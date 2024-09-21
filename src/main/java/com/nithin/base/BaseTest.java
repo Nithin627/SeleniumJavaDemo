60e3af7d4a9874d7ba3596c7fbe718eb87ea7f33
Nithin <nithinpaddyur@gmail.com>
2024-09-21 17:10:29 +0530

    Java seleium Second Commit by GitY

--- src/main/java/com/nithin/base/BaseTest.java
+++ src/main/java/com/nithin/base/BaseTest.java
@@ -4,6 +4,7 @@ public class BaseTest {
 	
 	public static void main(String[] args) {
 		System.out.println("First Test");
+		System.out.println("I am Y changed some code....Please continue");
 	}
 
 }
