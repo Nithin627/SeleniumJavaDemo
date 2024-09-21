797c4161e148415feea20c1643af4abc2906920e
Nithin <nithinpaddyur@gmail.com>
2024-09-21 17:15:35 +0530

    I m GitX commiting code after GitY

--- src/main/java/com/nithin/base/BaseTest.java
+++ src/main/java/com/nithin/base/BaseTest.java
@@ -5,6 +5,8 @@ public class BaseTest {
 	public static void main(String[] args) {
 		System.out.println("First Test");
 		System.out.println("I am Y changed some code....Please continue");
+		
+		System.out.println("I m GitX I chenged some code please check");
 	}
 
 }
