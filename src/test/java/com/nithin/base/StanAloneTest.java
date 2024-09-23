236b17fdfdc4825aa6bd60eae7ceb08a060bd0ad
Nithin <nithinpaddyur@gmail.com>
2024-09-23 13:06:16 +0530

    I am GitY I have updated the code GitX pls continue

--- src/test/java/com/nithin/base/StanAloneTest.java
+++ src/test/java/com/nithin/base/StanAloneTest.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -32,7 +34,7 @@ public class StanAloneTest {
 		driver.findElement(By.id("login")).click();
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".mb-3")));
+//		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".mb-3")));
 
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
 
@@ -40,8 +42,33 @@ public class StanAloneTest {
 				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst()
 				.orElse(null);
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
-		
-		
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+
+		boolean match = cartProducts.stream().anyMatch(product -> product.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+
+		Actions action = new Actions(driver);
+
+		action.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "India").build()
+				.perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+
+		driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		driver.close();
+
+//		driver.findElement(By.xpath("//body[1]/app-root[1]/app-dashboard[1]/app-sidebar[1]/nav[1]/ul[1]/li[4]/button[1]")).click();		
 
 	}
 
